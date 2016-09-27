3d799a025c6d581f70d0c2eb3297c9f52e1322d6
tpstar <hanlee12@gmail.com>
2016-09-26 23:51:41 -0400

    make models and migration tables

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,10 @@
 class Cart < ActiveRecord::Base
+  has_many :orders
+  has_many :users, :through => :orders
+
+  has_many :line_items
+  has_many :items, :through => :line_items
+
+  belongs_to :category
 
 end
--- db/migrate/20140805200233_create_orders.rb
+++ db/migrate/20140805200233_create_orders.rb
@@ -1,5 +1,10 @@
 class CreateOrders < ActiveRecord::Migration
   def change
-
+    create_table :orders do |t|
+      t.integer :user_id
+      t.integer :cart_id
+      t.string :status
+      t.integer :total
+    end
   end
 end
